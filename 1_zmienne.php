b91303c2ab491b8648d7aec292cbff5e4e1054ec
Samotyja <sekinetsu40k@gmail.com>
2020-10-17 12:10:29 +0200

    PHP

--- 1_zmienne.php
+++ 1_zmienne.php
@@ -19,8 +19,41 @@
     echo $prawda; //wartość true to 1
     
     $fałsz = false;
-    echo $fałsz; //false nic nie wyświetli
+    echo "$fałsz<br>"; //false nic nie wyświetli
 
     //integer
+    $bin = 0b1101; //13
+    $oct = 01101;
+    $dec = 13;
+    $hex = 0xA1;
+    echo "$bin<br>";
+    echo "$oct<br>";
+    echo "$dec<br>";
+    echo "$hex<br>";
+    //interpolacja
+    echo $hex, '<hr>';
+
+    echo 'a'.'b'.'c'; //wolniejszy sposób a dodać b dodać c
+    echo 'a','b','c'; //wyświetl a wyświetl b wyświetl c
+
+    //składnia heredoc
+
+    $name = "Anna";
+    echo <<<LABEL
     
+    <br>Imię:$name
+LABEL;
+
+    $imie = "Jakub";
+    $nazwisko = "Samotyja";
+    $miejscowosc = "Chocicza";
+    $urodzenia = "Poznań";
+    echo <<<DANE
+    <br><br><hr>
+    <u>Dane użytkownika</u>
+    <br>Imię i Nazwisko: $imie $nazwisko
+    <br>Miejscowość: $miejscowosc
+    <br>Miejsce urodzenia: $urodzenia
+
+DANE;
     ?>
