c88981731def2848d13be2b094b0a7238829f513
Samotyja <sekinetsu40k@gmail.com>
2020-10-17 11:18:20 +0200

    PHP

--- 1_zmienne.php
+++ 1_zmienne.php
@@ -1,3 +1,26 @@
 <?php
+    $name = 'Jan';
     echo'Test';
-?>
+    echo'Test<br>';
+    echo 'Imie: $name<br>';
+    echo "Imie: $name<br>";
+    //konkatenacja
+    $name = 'Jan'.' Kowalski';
+    echo "Imie: $name<br>";
+
+    //dodawanie 
+    $add = 2 + 3;
+    echo "$add<br>";
+
+    //typy danych
+    //boolen
+
+    $prawda = true;
+    echo $prawda; //wartość true to 1
+    
+    $fałsz = false;
+    echo $fałsz; //false nic nie wyświetli
+
+    //integer
+    
+    ?>
