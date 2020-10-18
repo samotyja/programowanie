d0a9cab6d245e491ce564c867b8e506a45a76b3a
Samotyja <sekinetsu40k@gmail.com>
2020-10-18 12:54:38 +0200

    PHP 18.10

--- 5_form/1_zadanie.php
+++ 5_form/1_zadanie.php
@@ -9,13 +9,32 @@
     <h1>Formularz z danymi użytkowników</h1>
     <meta name="viewport" content="width">
     <form action="./2_zadanie_dane.php" method="get">
-    <input type="text" name="name" placeholder="Imię"><br>
-    <input type="text" name="surname" placeholder="Nazwisko"><br>
-    <input type="password" name="pass" placeholder="Hasło"><br>
+    <?php
+        if(!empty($_GET['name'])){
+            $name=$_GET['name'];
+            echo "<input type=\"text\" name=\"name\" placeholder=\"imię\" value=\"$name\"><br><br>";
+        }else{
+            echo '<input type="text" name="name" placeholder="Imię"><br><br>';
+        }
+    ?>
+    <?php
+    if(!empty($_GET['surname'])){
+            $surname=$_GET['surname'];
+            echo "<input type=\"text\" name=\"surname\" placeholder=\"nazwisko\" value=\"$surname\"><br><br>";
+        }else{
+            echo '<input type="text" name="surname" placeholder="Nazwisko"><br><br>';
+        }
+    ?>
+    <input type="password" name="pass" placeholder="Hasło"><br><br>
     <input type="text" name="kod1" size="1" maxlength="2"> - <input type="text" name="kod2" size="2" maxlength="3">
-
+    <h4>Ulubiony kolor</h4>
+    <input type="radio" name="color" value="r" checked>Czerwony
+    <input type="radio" name="color" value="g">Zielony
+    <input type="radio" name="color" value="b">Niebieski<br><br>
     <br><br>
+    <input type="checkbox" name="statute">Regulamin<br><br>
     <input type="submit" value="Wyślij dane">
-
+    
+</form>
 </body>
 </html>
--- 5_form/2_zadanie_dane.php
+++ 5_form/2_zadanie_dane.php
@@ -12,20 +12,37 @@
     //print_r($_GET); //wyświetla wszystkie przesłane dane
     // echo $_GET['surname'];
     //operatory logiczne AND &&, OR ||
-    if (/*isset*/!empty($_GET['surname'])&& !empty($_GET['pass'])&& !empty($_GET['name'])&& !empty($_GET['kod1'])&& !empty($_GET['kod2'])) {
+    if (/*isset*/!empty($_GET['surname'])&& !empty($_GET['pass'])&& !empty($_GET['name'])&& !empty($_GET['kod1'])&& !empty($_GET['kod2'])&& !empty($_GET['kod1'])&& !empty($_GET['color'])&&($_GET['statute']=='on')) {
+        $name = $_GET['name'];
+        $surname= $_GET['surname'];
         echo 'imie wprowadzone<br>';
+        $color="";
+        switch ($_GET['color']) {
+        case 'r':
+            $color="Czerwony";
+        break;
+        case 'g':
+            $color="Zielony";
+        break;
+        default:
+            $color="Niebieski";
+    break;
+}
     echo <<<T
     Imię i Nazwisko: $_GET[surname]$_GET[name]<br>
     Hasło: $_GET[pass] <br>
-    Kod pocztowy: $_GET[kod1]-$_GET[kod2]
+    Kod pocztowy: $_GET[kod1]-$_GET[kod2]<br>
+    
+    Ulubiony kolor: $color<hr>
 T;
+    echo "<a href=\"./1_zadanie.php?name=$name&surname=$surname\">Popraw dane w formularzu</a>";
     }else{
-        //header("Location:./1_zadanie.php");
+       // header("Location:./1_zadanie.php");
     
 
     ?>
     <script>
-        history.back();
+       history.back();
     </script>
     
     <?php
